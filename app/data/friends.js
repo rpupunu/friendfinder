574eb55f682a0d74a055b5bb79cdb628919f47ec
rpupunu <rpupunu@gmail.com>
2019-11-13 16:57:40 -0700

    Add friends objects, url, and values

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,79 @@
-let friends = [{
-    "name": "name",
-    "photo": "url",
+let friends = [
+    {
+    "name": "Bill Clinton",
+    "photo": "https://en.wikipedia.org/wiki/Bill_Clinton#/media/File:Bill_Clinton.jpg",
     "scores": [
-        1, 2, 3, 4, 5, 6, 7, 8, 9
-    ]
-}]
+        4,
+        4, 
+        4, 
+        4, 
+        4, 
+        4, 
+        4, 
+        4, 
+        4, 
+        4
+    ]},
+    {
+        "name": "George HW. Bush",
+        "photo": "https://en.wikipedia.org/wiki/George_H._W._Bush#/media/File:George_H._W._Bush,_President_of_the_United_States,_1989_official_portrait_(cropped).jpg",
+        "scores": [
+            3, 
+            3,
+            3, 
+            3, 
+            3, 
+            3, 
+            3, 
+            3, 
+            3, 
+            3   
+    ]},
+    {
+        "name": "George W. Bush",
+        "photo": "https://en.wikipedia.org/wiki/George_W._Bush#/media/File:George-W-Bush.jpeg",
+        "scores": [
+            2, 
+            2, 
+            2, 
+            2, 
+            2, 
+            2, 
+            2, 
+            2, 
+            2,
+            2   
+    ]},
+    {
+        "name": "Barack Obama",
+        "photo": "https://en.wikipedia.org/wiki/Barack_Obama#/media/File:President_Barack_Obama.jpg",
+        "scores": [
+            5, 
+            5, 
+            5, 
+            5, 
+            5, 
+            5, 
+            5, 
+            5, 
+            5,
+            5
+    ]},
+    {
+        "name": "Donald J Trump",
+        "photo": "https://en.wikipedia.org/wiki/Donald_Trump#/media/File:Donald_Trump_official_portrait.jpg",
+        "scores": [
+            1, 
+            1, 
+            1, 
+            1, 
+            1, 
+            1, 
+            1, 
+            1, 
+            1,
+            1
+    ]},
+];
 
 module.exports = friends;
