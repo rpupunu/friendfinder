c77da53ce6c87c899b4f36cfdd0a8ec93ae78d60
rpupunu <rpupunu@gmail.com>
2019-11-13 16:01:21 -0700

    finished server.js file to require routing js files for page displays

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ var PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+// REQUIRE THE ROUTING JS FILES TO HANDLE ROUTES
+
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
 
 // Start the server
 app.listen(PORT, function() {
